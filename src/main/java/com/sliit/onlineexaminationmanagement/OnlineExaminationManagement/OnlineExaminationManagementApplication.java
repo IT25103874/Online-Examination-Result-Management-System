a7fa7b28db55dbe255a398633462ec5851c7a75b
IT25103874 <it25103874@my.sliit.lk>
2026-04-27 16:44:00 +0530

    Initial Commit

---
+++ src/main/java/com/sliit/onlineexaminationmanagement/OnlineExaminationManagement/OnlineExaminationManagementApplication.java
@@ -0,0 +1,13 @@
+package com.sliit.onlineexaminationmanagement.OnlineExaminationManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineExaminationManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineExaminationManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sliit/onlineexaminationmanagement/OnlineExaminationManagement/OnlineExaminationManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sliit.onlineexaminationmanagement.OnlineExaminationManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineExaminationManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
