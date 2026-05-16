d128ee82375d1561a160f435cf883f18d7e7ece0
IT25103868 <it25103868@my.sliit.lk>
2026-05-17 01:09:45 +0530

    updated subject repository

---
+++ src/main/java/com/sliit/onlineexaminationmanagement/OnlineExaminationManagement/repository/SubjectRepository.java
@@ -0,0 +1,14 @@
+package com.sliit.onlineexaminationmanagement.OnlineExaminationManagement.repository;
+
+import com.sliit.onlineexaminationmanagement.OnlineExaminationManagement.entity.Subject;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface SubjectRepository extends JpaRepository<Subject, Long> {
+    Optional<Subject> findBySubjectCode(String subjectCode);
+    boolean existsBySubjectCode(String subjectCode);
+    List<Subject> findByCourseId(Long courseId);
+}
