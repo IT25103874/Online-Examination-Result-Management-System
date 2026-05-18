8df025d548e516e2694aba3a369b59df739e3b05
it25103873 <it25103873@my.sliit.lk>
2026-05-19 01:25:04 +0530

    implement question and paper management function

---
+++ src/main/java/com/sliit/onlineexaminationmanagement/OnlineExaminationManagement/repository/QuestionRepository.java
@@ -0,0 +1,13 @@
+package com.sliit.onlineexaminationmanagement.OnlineExaminationManagement.repository;
+
+import com.sliit.onlineexaminationmanagement.OnlineExaminationManagement.entity.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface QuestionRepository extends JpaRepository<Question, Integer> {
+    List<Question> findBySubjectSubjectIdAndDifficultyLevel(Integer subjectId, String difficultyLevel);
+
+}
