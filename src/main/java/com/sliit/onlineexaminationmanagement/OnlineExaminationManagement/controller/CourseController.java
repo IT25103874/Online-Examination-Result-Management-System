d28998832ffdc5b6ed33944e01f3663d0b5a39d4
IT25103872 <it25103872@my.sliit.lk>
2026-05-21 09:42:23 +0530

    Update CourseController.java

--- src/main/java/com/sliit/onlineexaminationmanagement/OnlineExaminationManagement/controller/CourseController.java
+++ src/main/java/com/sliit/onlineexaminationmanagement/OnlineExaminationManagement/controller/CourseController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/courses") // 👈 Fixed: Added /v1 to match your Postman requests
+@RequestMapping("/api/v1/courses") 
 @CrossOrigin(origins = "*")
 public class CourseController {
 
