41931bc00095779ace0317d167e7e188d979fce3
IT25103874 <it25103874@my.sliit.lk>
2026-05-21 09:59:27 +0530

    Update ExamService.java

--- src/main/java/com/sliit/onlineexaminationmanagement/OnlineExaminationManagement/service/ExamService.java
+++ src/main/java/com/sliit/onlineexaminationmanagement/OnlineExaminationManagement/service/ExamService.java
@@ -5,13 +5,13 @@ import com.sliit.onlineexaminationmanagement.OnlineExaminationManagement.entity.
 import java.util.List;
 
 public interface ExamService {
-    //Validates, creates, and schedules a new examination in the system.
+    
     Exam createAndScheduleExam(Exam exam);
     List<Exam> getAllExams();
     Exam getExamById(int id);
     void cancelExam(int id);
 
-    // New Additions
+   
     Exam updateExam(int id, Exam updatedExam);
     void deleteExam(int id);
     List<Exam> searchExamsByTitle(String title);
